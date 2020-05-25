a941422288e0d218e9badc3b0319476df64153ce
github username <github email address>
2020-05-25 10:35:28 -0600

    Done.

--- bin/environment.rb
+++ bin/environment.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 require 'bundler/setup'
 Bundler.require
 require 'rake'
 require 'active_record'
+require 'pry'
 
 require_relative '../lib/dog'
 require_relative 'sql_runner'
 
 DB = ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "./db/dogs.db"
+  adapter: 'sqlite3',
+  database: './db/dogs.db'
 )
 
 DB = ActiveRecord::Base.connection
 
-if ENV["ACTIVE_RECORD_ENV"] == "test"
-  ActiveRecord::Migration.verbose = false
-end
+ActiveRecord::Migration.verbose = false if ENV['ACTIVE_RECORD_ENV'] == 'test'
